6199eea54d06cd93984b8274ecd96d6af7f23ba1
shahil rawat <shahilr632@gmail.com>
2026-06-20 17:26:41 +0530

    added functionality for exchange rate button using exchange api

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let BASE_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_Ud3GCkckKGv1Yxx92WbovBzZnzT3RDF2Ez4sQ2yR&currencies=EUR%2CUSD%2CCAD"
-let dropdown = document.querySelectorAll(".dropDown select");
-let btn = document.querySelector("button");
+const BASE_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_Ud3GCkckKGv1Yxx92WbovBzZnzT3RDF2Ez4sQ2yR"
+const dropdown = document.querySelectorAll(".dropDown select");
+const btn = document.querySelector("button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".To select");
+const msg = document.querySelector(".msg");
 
 
 for (let select of dropdown){
@@ -24,10 +27,34 @@ for (let select of dropdown){
 const flag = (element)=>{
     let currCode = element.value
     let countryCode = countryList[currCode];
-    console.log(countryCode)
+    // console.log(countryCode)
     let img = element.parentElement.querySelector("img");
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
     img.src = newSrc;
 }
+
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();
+    let input = document.querySelector("input");
+    inputVal = input.value;
+    // console.log(inputVal);
+    if(inputVal === "" || inputVal<1){
+        inputVal = 1;
+        input.value = 1;
+    }
+
+    const URL = `https://api.currencyapi.com/v3/latest?apikey=cur_live_Ud3GCkckKGv1Yxx92WbovBzZnzT3RDF2Ez4sQ2yR&base_currency=${fromCurr.value}&currencies=${toCurr.value}`
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.data[toCurr.value].value;
+    
+    let finalAmt = inputVal * rate;
+
+    msg.innerText = `${inputVal}${fromCurr.value} = ${finalAmt}${toCurr.value}`;
+
+    msg.classList.remove("hide");
+    
+})
 
 
